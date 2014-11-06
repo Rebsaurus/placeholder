9f03c787f01808282188823dca53ca861854a0c3
Kleven <likleven@gmail.com>
2014-11-06 13:47:18 +0100

    Prøvd å komme igang litt, mockup til vi får henta ut lister over distrikter / klinikker

---
+++ qa.js
@@ -0,0 +1,8 @@
+function dropDowns(){
+document.getElementById("districtDropdown").selectedIndex = -1;
+document.getElementById("clinicDropdown").selectedIndex = -1;
+}
+
+function popup(){
+	alert("test!")
+}

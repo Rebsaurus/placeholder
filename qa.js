11975ef8446427b3e1d6cae4e48667c2a912fdf1
Kleven <likleven@gmail.com>
2014-11-17 11:34:46 +0100

    Test Merge

--- qa.js
+++ qa.js
@@ -1,8 +1,23 @@
-function dropDowns(){
-document.getElementById("districtDropdown").selectedIndex = -1;
-document.getElementById("clinicDropdown").selectedIndex = -1;
+/*var district = ["Aclinic" "Bclinic", "Cclinic"];*/
+
+function showClinic(){
+	var selectedDistrict = document.getElementById("districtSelect").value;
+	if (selectedDistrict > 0){
+		document.getElementById("clinicSelect").style.display = "block";
+	}
 }
 
+function showTable(){
+	document.getElementById("qaTable").style.display = "block";
+	var chosenClinic = document.getElementById("clinicOpts").value;
+	document.getElementById("chosenClinic").innerHTML = chosenClinic;
+
+	/*if (window.innerWidth < 481){
+		document.getElementById("selector").style.display = "none";
+	}*/
+}
+
+
 function popup(){
-	alert("test!")
+	alert("test!");
 }

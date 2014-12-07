5be7263e09b1fca689383fe559f8a5e8bb67790e
Michelle Cheung <michelch@matnat.uio.no>
2014-12-07 15:24:31 +0100

    fikset css i statestikk siden og tabellen

--- statistics.js
+++ statistics.js
@@ -5,16 +5,16 @@ function percentCalc() {
 		var altArray = data;
 		//gar gjennom array[0], som er klinikkobjekter
 		for (var key in altArray) {
-			string = '<table border="1" style="width:60%"><tr><tr><th colspan="2">' + altArray[key].clinic.name + '</tr></thead><tbody>';
+			string = '<table border="1" style="width:60%"><tr><tr><th colspan="3">' + altArray[key].clinic.name + '</tr></thead><tbody>';
 
-			string = string + '<tr><td>Form</td><td>Data Completed</td></tr>';
+			string = string + '<tr style="background-color:#7aa6d3"><td style="width:50%">Form</td><td>Number of submits</td><td>Data Consistency</td></tr>';
 			//gar gjennom alle klinikkene sine forms
 			for (var key2 in altArray[key].clinic.forms) {
 				
 				var totalR = altArray[key].clinic.forms[key2].formTotalRows;
 				var completeR = altArray[key].clinic.forms[key2].formCompleteRows;
 				
-				string = string + '<tr><td>' + altArray[key].clinic.forms[key2].name + '</td><td>' + (completeR/totalR*100).toFixed(2) + '%</td></tr>';
+				string = string + '<tr><td>' + altArray[key].clinic.forms[key2].name + '</td><td>' + altArray[key].clinic.forms[key2].submitCount + '</td><td>' + (completeR/totalR*100).toFixed(2) + '%</td></tr>';
 				var rCount = altArray[key].clinic.forms[key2].rows;
 				var hCount = altArray[key].clinic.forms[key2].coloumns;
 				//gar gjennom hver form sine submits

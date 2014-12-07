c48ac6dcb34ac45afb2debbb61a638ef082797db
Michelle Cheung <michelch@matnat.uio.no>
2014-12-07 18:00:12 +0100

    Fikset text-align i statestikk tabellen

--- statistics.js
+++ statistics.js
@@ -7,14 +7,14 @@ function percentCalc() {
 		for (var key in altArray) {
 			string = '<table border="1" style="width:60%"><tr><tr><th colspan="3">' + altArray[key].clinic.name + '</tr></thead><tbody>';
 
-			string = string + '<tr style="background-color:#7aa6d3"><td style="width:50%">Form</td><td>Number of submits</td><td>Data Consistency</td></tr>';
+			string = string + '<tr style="background-color:#7aa6d3"><td style="width:50%" class="alignleft">Form</td><td>Number of submits</td><td>Data Consistency</td></tr>';
 			//gar gjennom alle klinikkene sine forms
 			for (var key2 in altArray[key].clinic.forms) {
 				
 				var totalR = altArray[key].clinic.forms[key2].formTotalRows;
 				var completeR = altArray[key].clinic.forms[key2].formCompleteRows;
 				
-				string = string + '<tr><td>' + altArray[key].clinic.forms[key2].name + '</td><td>' + altArray[key].clinic.forms[key2].submitCount + '</td><td>' + (completeR/totalR*100).toFixed(2) + '%</td></tr>';
+				string = string + '<tr><td class="alignleft">' + altArray[key].clinic.forms[key2].name + '</td><td>' + altArray[key].clinic.forms[key2].submitCount + '</td><td>' + (completeR/totalR*100).toFixed(2) + '%</td></tr>';
 				var rCount = altArray[key].clinic.forms[key2].rows;
 				var hCount = altArray[key].clinic.forms[key2].coloumns;
 				//gar gjennom hver form sine submits
